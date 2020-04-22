875d56714255cb72700850e4e9ad8e986a29256a
Chris Hosler <cmstie@gmail.com>
2020-04-22 18:19:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,11 @@ require "yaml"
 
 def load_library(file_path)
   emoticon = YAML.load_file(file_path)
+  new_hash = {}
   
+  emoticon.each do |name, symbols|
+    name.each do |symbols2|
+      symbols2[0] 
 end
 
 def get_japanese_emoticon
