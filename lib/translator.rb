17dd32925b3d0b4a460dfd873f39d980f5f5b8ca
Chris Hosler <cmstie@gmail.com>
2020-04-22 18:56:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,11 +12,11 @@ def load_library(file_path)
     new_hash
 end
 
-def get_japanese_emoticon(file_path, eng_emot)
+def get_japanese_emoticon(file_path, emot)
   emot_lib = load_library(file_path)
-  
+  emot_lib
 end
 
-def get_english_meaning(file_path, jp_emot)
+def get_english_meaning(file_path, emot)
   
 end
