31fb9eb1b6ee85e333e2d7ac85fb861b34ff8dc9
Chris Hosler <cmstie@gmail.com>
2020-04-22 19:01:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,6 +15,10 @@ end
 def get_japanese_emoticon(file_path, emot)
   emot_lib = load_library(file_path)
   emot = emot_lib.keys.find do |name|
+    if emot_lib[name][:english] == emot
+      return emot_lib[name][:japanese]
+    else return "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning(file_path, emot)
